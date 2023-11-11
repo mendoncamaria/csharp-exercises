e7d9acf502329d9d674f45ec43c591dee417051c
Maria Mendonca <71168380+mendoncamaria@users.noreply.github.com>
2023-11-11 14:14:22 +0530

    Create FraudOrderList.cs

---
+++ TestProject/FraudOrderList.cs
@@ -0,0 +1,8 @@
+string[] fraudulentOrderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string id in fraudulentOrderIDs)
+{
+    if(id.StartsWith("B")){
+        Console.WriteLine(id);
+    }
+}
