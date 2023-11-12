bd08e9f6278af697945cfb36e6e90f53d092b85b
Maria Mendonca <71168380+mendoncamaria@users.noreply.github.com>
2023-11-12 11:39:29 +0530

    Create fizzBuzz.cs

---
+++ TestProject/fizzBuzz.cs
@@ -0,0 +1,13 @@
+// If number divisible by 3, then Fizz, if divisible by 5, Buzz, and both 3 and 5 is FizzBuzz
+
+for (int i = 1; i < 101; i++)
+{
+    if ((i % 3 == 0) && (i % 5 == 0))
+        Console.WriteLine($"{i} - FizzBuzz");
+    else if (i % 3 == 0)
+        Console.WriteLine($"{i} - Fizz");
+    else if (i % 5 == 0)
+        Console.WriteLine($"{i} - Buzz");
+    else
+        Console.WriteLine($"{i}");
+}
